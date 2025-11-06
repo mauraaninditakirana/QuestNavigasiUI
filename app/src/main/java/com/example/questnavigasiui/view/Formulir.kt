55b44729c254296c866078bda311632b93e2c5e6
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-06 13:11:34 +0700

    menambahkan composable FormIsian dalam Formulir.kt

--- app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
@@ -1,2 +1,16 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.questnavigasiui.view
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSUbmitBtnClick: () -> Unit
+){
+
+}
 
