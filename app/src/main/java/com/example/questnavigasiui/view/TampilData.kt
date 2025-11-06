a3897a34b703b75153a03974f733fe8169f374e2
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-06 13:56:40 +0700

    Membuat TampilanData dan menambahkan Scaffold dengan TopAppBar

--- app/src/main/java/com/example/questnavigasiui/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui/view/TampilData.kt
@@ -1,12 +1,33 @@
 package com.example.questnavigasiui.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigasiui.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     onBackBtnClick:()->Unit
 ){
-
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
+    )
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = {Text(text = stringResource(id = R.string.tampilan), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.teal_700)),
+                )
+            }
+    ){}
 }
