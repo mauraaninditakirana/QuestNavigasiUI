70c5fd69f69f64f0d32e32f643c3e736ab2840f8
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-06 13:45:00 +0700

    Membuat Button dan Spacer di dalam file Formulir.kt

--- app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
@@ -5,8 +5,12 @@ package com.example.questnavigasiui.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -84,6 +88,13 @@ fun FormIsian(
                     label = { Text(text = "Alamat")},
                     onValueChange = {}
                 )
+                Spacer(modifier = Modifier.height(30.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(1f),
+                    onClick = OnSUbmitBtnClick
+                ){
+
+                }
             }
 
     }
