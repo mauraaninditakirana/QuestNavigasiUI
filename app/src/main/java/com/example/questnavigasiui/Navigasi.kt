31ee0cb45278d3fe6881962d0d7c6a860fc0a35a
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-06 12:07:19 +0700

    Membuat enum Navigasi dan composable DataApp

--- app/src/main/java/com/example/questnavigasiui/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui/Navigasi.kt
@@ -1,2 +1,17 @@
 package com.example.questnavigasiui
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+) {}
