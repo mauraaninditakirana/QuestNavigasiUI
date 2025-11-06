00daf9607dd864410beab8c9f31abd310d5dd493
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-06 13:21:48 +0700

    Menambahkan TopAppBar dengan judul dan warna latar belakang.

--- app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
@@ -3,10 +3,16 @@
 package com.example.questnavigasiui.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.R
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun FormIsian(
@@ -15,7 +21,12 @@ fun FormIsian(
 ){
     Scaffold (modifier= Modifier,
         {
-            TopAppBar()
-        })
+            TopAppBar(
+                title = {Text(stringResource(id= R.string.home),
+                    color = Color.White)},
+                colors = TopAppBarDefaults.topAppBarColors
+                    (colorResource(id = R.color.teal_700))
+            )}
+        ){}
 }
 
