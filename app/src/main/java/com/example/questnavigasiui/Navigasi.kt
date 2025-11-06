1ecccb58fb85c2befec86ca8876d32469edd515f
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-06 12:13:59 +0700

    Membuat NavHost dan Scaffold untuk mengatur navigasi antar halaman.

--- app/src/main/java/com/example/questnavigasiui/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui/Navigasi.kt
@@ -1,8 +1,12 @@
 package com.example.questnavigasiui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
@@ -14,4 +18,21 @@ enum class Navigasi {
 fun DataApp(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier
-) {}
+){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(isiRuang)){
+            composable(route = Navigasi.Formulirku.name){
+                FormIsian(
+                    //pilihanJK = JenisK.map {id -> konteks.resources.getString(id)},
+                    OnSubmitButtonCliked = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+        }
+    }
+}
